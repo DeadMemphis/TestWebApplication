5a0a62dc7cfdf8596667f02f749a1b8f41764826
DEADMEMPHIS <krymsk_795@mail.ru>
2020-11-06 20:37:49 +0300

    Add project files.

---
+++ TestWebApplication/JsonTool/JsonRead.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace TestWebApplication.JsonTool
+{
+    public class JsonRead
+    {
+        public string Read(string fileName, string location)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            root,
+            location,
+            fileName);
+
+            string jsonResult;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                jsonResult = sr.ReadToEnd();
+            }
+            return jsonResult;
+        }
+    }
+}
---
+++ TestWebApplication/JsonTool/JsonWrite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace TestWebApplication.JsonTool
+{
+    public class JsonWrite
+    {
+        public void Write(string fileName, string location, string jstring)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            root,
+            location,
+            fileName);
+
+            using (var sw = File.CreateText(path))
+            {
+                sw.Write(jstring);
+            }
+        }
+    }
+}
